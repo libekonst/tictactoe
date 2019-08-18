1faee307213564feb02e0755313d7f2a79f636e8
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-08-18 23:43:59 +0300

    Updates

---
+++ precache-manifest.e4160c3e702b0408cb309cbbb64aecfa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88dfa58593eb46958638dca28666568f",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "77361f33feea59ca32e1",
+    "url": "/tictactoe/static/css/main.58b4ae99.chunk.css"
+  },
+  {
+    "revision": "f583fc4ba18881744376",
+    "url": "/tictactoe/static/js/2.dd4e8116.chunk.js"
+  },
+  {
+    "revision": "77361f33feea59ca32e1",
+    "url": "/tictactoe/static/js/main.20b43634.chunk.js"
+  },
+  {
+    "revision": "da3a7676ccdbb1a27898",
+    "url": "/tictactoe/static/js/runtime~main.1d602e93.js"
+  },
+  {
+    "revision": "693ff6e79ede418cfdcc642b70e93814",
+    "url": "/tictactoe/static/media/gameover.693ff6e7.mp3"
+  },
+  {
+    "revision": "e15c5d19d1a5135a951f47e27b72282d",
+    "url": "/tictactoe/static/media/music.e15c5d19.mp3"
+  },
+  {
+    "revision": "8cde2f6b67a62e1ff8a583da745519f3",
+    "url": "/tictactoe/static/media/refresh.8cde2f6b.svg"
+  },
+  {
+    "revision": "3f8924a4a165adf6ad98b6a22e694f0d",
+    "url": "/tictactoe/static/media/volume-off.3f8924a4.svg"
+  },
+  {
+    "revision": "e6445464e4e55d0aa413577fd8e1bdf9",
+    "url": "/tictactoe/static/media/volume-on.e6445464.svg"
+  }
+]);
